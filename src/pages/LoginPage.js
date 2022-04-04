9e9e9ef87263ac2edd915cf16e1bee80947c2503
Trang Luong <luongtrang014@gmail.com>
2022-04-04 16:10:38 +0800

    update

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from "../components/logo";
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>LoginPage</h1>
+      <Logo sx={{ width: 200, height: 200 }} />
+    </div>
+  );
+}
+
+export default LoginPage;
