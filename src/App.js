c97a239047dcab2bfca9c56c38917370afadf3a4
Trang Luong <luongtrang014@gmail.com>
2022-04-18 03:18:55 +0800

    update style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Router from "./routes";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import CartContextProvider from "./contexts/CartContext";
+import "./App.css";
 
 function App() {
   return (
--- src/pages/CheckoutCompletedPage.js
+++ src/pages/CheckoutCompletedPage.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function CheckoutCompletedPage() {
   return (
-    <div>
-      <h1>Thank you for your purchase.</h1>
+    <div className="checkout-completed">
+      <div>
+        <h1>Thank you for your purchase.</h1>
+      </div>
     </div>
   );
 }
